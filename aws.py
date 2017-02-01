d3c9ba99d547a8fb954a0e6f339c48e12fcf985a
Gary <gazbo83@gmail.com>
2017-02-01 12:52:15 +0000

    oops

--- aws.py
+++ aws.py
@@ -1,8 +1,30 @@
-import boto3
+import boto
+from boto.s3.connection import Location
 
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
-    total_size = 0
-    for key in bucket.list():
-        total_size += key.size
+s3 = boto.connect_s3()
+bucket = s3.lookup('osq')
+
+try:
+   read_bytes = open('currbytes', 'r')
+except:
+   print 'Failed to open file'
+pass
+
+
+total_bytes = 0
+for key in bucket:
+    total_bytes += key.size
+print total_bytes
+
+for i in read_bytes:
+    fluff = int(i)
+    print fluff
+    if total_bytes <= fluff:
+        print "S3 storage isn't growing"
+    elif total_bytes > fluff:
+        print "S3 storage growing"
+    else:
+        print "Something else is going on ",'%d %d' (total_bytes,i)
+
+write_bytes = open('currbytes', 'w')
+write_bytes.write(str(total_bytes))
