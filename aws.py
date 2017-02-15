afaaec3c9fea5addff0abb387ce703c51aae1cab
Gary <gazbo83@gmail.com>
2017-02-15 09:28:18 +0000

    mongoed

--- aws.py
+++ aws.py
@@ -8,37 +8,18 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.AWSdb
 s3 = boto.connect_s3()
-bucket = s3.lookup(str(sys.argv[1]))
-total_bytes = 0
 
-try:
-   read_bytes = open('currbytes', 'r+')
-except:
-   print 'Creating temp file'
-   read_bytes = open('currbytes', 'w')
-   read_bytes.write(str(total_bytes))
-pass
+for i in sys.argv[1:]:
+    print i
+    total_bytes = 0
+    bucket = s3.lookup(str(i))
+    print bucket
+    for key in bucket:
+        total_bytes += key.size
+    print total_bytes
 
-for key in bucket:
-    total_bytes += key.size
-print total_bytes
+    date=datetime.datetime.utcnow()
+    db.sizes.insert({"total" : total_bytes,"bucket" : i,'date': date})
 
-for i in read_bytes:
-    fluff = int(i)
-    print fluff
-    if total_bytes <= fluff:
-        print "S3 storage isn't growing"
-    elif total_bytes > fluff:
-        print "S3 storage growing"
-    else:
-        print "Something else is going on, previous: %d current: %d" (total_bytes,i)
-
-read_bytes.seek(0)
-read_bytes.truncate()
-read_bytes.write(str(total_bytes))
-date=datetime.datetime.utcnow()
-db.sizes.insert({"total" : total_bytes,"bucket" : sys.argv[1],'date': date})
-print db.sizes.find_one()
-for r in db.sizes.find({"bucket" : sys.argv[1]}):
+for r in db.sizes.find():
     print r
-read_bytes.close()
